473a0a7a3bcbf3f0e84411900adfc7b15d12cee0
yot0201412 <laughing.my.way0201@gmail.com>
2020-04-05 16:17:17 +0900

    add RaseList

--- HomePages/Items/BeforeInfoRacer.py
+++ HomePages/Items/BeforeInfoRacer.py
@@ -2,10 +2,10 @@ import re
 
 # 直前情報のレーサーごとの情報を保持するクラス
 class BeforeInfoRacer:
-    number_index = 1
-    weight_index = 3
-    exhibition_index = 4
-    tilt_index = 5
+    NUMBER_INDEX = 1
+    WEIGHT_INDEX = 3
+    EXHIBITION_INDEX = 4
+    TILT_INDEX = 5
     
     def __init__(self, elm):
         self.elm = elm
@@ -16,16 +16,16 @@ class BeforeInfoRacer:
         self.scrape()
         
     def scrape(self):
-        self.pase_elm()
+        self._pase_elm()
         print("number: %s, weight: %s, exb: %s, tilt: %s" % (self.number, self.weight, self.exhibition, self.tilt))
     
-    def pase_elm(self):
-        tds = self.elm.select_one("tr").select("td")
-        self.weight = tds[BeforeInfoRacer.weight_index].get_text()
-        self.exhibition = tds[BeforeInfoRacer.exhibition_index].get_text()
-        self.tilt = tds[BeforeInfoRacer.tilt_index].get_text()
-        src = tds[BeforeInfoRacer.number_index].select_one("img")['src']
-        self.number = self.parse_src(src)
+    def _pase_elm(self):
+        _tds = self.elm.select_one("tr").select("td")
+        self.weight = _tds[BeforeInfoRacer.WEIGHT_INDEX].get_text()
+        self.exhibition = _tds[BeforeInfoRacer.EXHIBITION_INDEX].get_text()
+        self.tilt = _tds[BeforeInfoRacer.TILT_INDEX].get_text()
+        _src = _tds[BeforeInfoRacer.NUMBER_INDEX].select_one("img")['src']
+        self.number = self._parse_src(_src)
     
-    def parse_src(self, string):
+    def _parse_src(self, string):
         return re.findall('/racerphoto/(.*).jpg', string)[0]
--- run_sc.py
+++ run_sc.py
@@ -1,6 +1,7 @@
 import requests
 from HomePages.BeforeInfo import BeforeInfo
 from HomePages.RaceIndex import RaceIndex
+from HomePages.RaceList import RaceList
 
 # 事前情報取得
 # r = requests.get('http://www.boatrace.jp/owpc/pc/race/beforeinfo?rno=1&jcd=06&hd=20200329')
@@ -9,15 +10,18 @@ from HomePages.RaceIndex import RaceIndex
 
 # ある日のあるレース会場のレース情報を保持する
 # レースある日
-r = requests.get('http://www.boatrace.jp/owpc/pc/race/raceindex?jcd=06&hd=20200329')
+# r = requests.get('http://www.boatrace.jp/owpc/pc/race/raceindex?jcd=06&hd=20200329')
 # レースない日
 # r = requests.get('http://www.boatrace.jp/owpc/pc/race/raceindex?jcd=06&hd=20200330')
 
-rp = RaceIndex(r)
+# rp = RaceIndex(r)
+# rp.scrape()
+# if rp.has_race():
+#     print(rp.race_urls)
+# else:
+#     print("no race")
+    
+r = requests.get('http://www.boatrace.jp/owpc/pc/race/racelist?rno=1&jcd=06&hd=20200329')
+rp = RaceList(r)
 rp.scrape()
-if rp.has_race():
-    print(rp.race_urls)
-else:
-    print("no race")
-
 print("end")
